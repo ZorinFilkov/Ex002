fff35cac76fbc39c6bd2a3a702b0dcbf9119e7de
ZorinFilkov <zfilkov@abv.bg>
2016-02-15 21:14:19 +0200

    Second example

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int nm = 51;
+		System.out.println(nm);
+
+		int nm1 = nm;
+		System.out.println(nm1);
+		
+		nm=102;
+		System.out.println(nm);
+		System.out.println(nm1);
+
+	}
+
+}
